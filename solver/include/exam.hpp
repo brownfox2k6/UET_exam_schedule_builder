00b416441df3942c1a7dbbd16c0bb1b31fbc7e8f
Nguyễn Trọng Đại <brfox2k6@gmail.com>
2026-05-15 04:34:39 +0700

    Update exam.hpp

--- solver/include/exam.hpp
+++ solver/include/exam.hpp
@@ -1,2 +1,24 @@
 #pragma once
 
+#include <utility>
+#include <vector>
+
+struct Exam {
+  int student_count;
+  std::vector<int> feasible_slots;
+  std::vector<int> feasible_rooms;
+  std::vector<int> feasible_proctors;
+
+  Exam() = default;
+
+  Exam(
+    int _student_count,
+    std::vector<int> _feasible_slots,
+    std::vector<int> _feasible_rooms,
+    std::vector<int> _feasible_proctors
+  ) : student_count(_student_count),
+      feasible_slots(std::move(_feasible_slots)),
+      feasible_rooms(std::move(_feasible_rooms)),
+      feasible_proctors(std::move(_feasible_proctors))
+  {}
+};
