5d179d680a0e5b5409cb683338955a95f362ccb5
Nguyễn Trọng Đại <brfox2k6@gmail.com>
2026-08-25 18:10:29 +0700

    Migrate from pybind11 to nanobind

--- solver/src/bindings.cpp
+++ solver/src/bindings.cpp
@@ -1,10 +1,6 @@
-#include <pybind11/attr.h>
-#include <pybind11/cast.h>
-#include <pybind11/functional.h>
-#include <pybind11/gil.h>
-#include <pybind11/pybind11.h>
-#include <pybind11/pytypes.h>
-#include <pybind11/stl.h>
+#include <nanobind/stl/function.h>
+#include <nanobind/stl/string.h>
+#include <nanobind/stl/vector.h>
 
 #include <cstdint>
 
@@ -14,97 +10,97 @@
 #include "common/room.hpp"
 #include "common/solution.hpp"
 
-namespace py = pybind11;
+namespace nb = nanobind;
 
-PYBIND11_MODULE(aco_solver, m) {  // NOLINT(readability-identifier-length)
+NB_MODULE(aco_solver, m) {  // NOLINT(readability-identifier-length)
   m.doc() =
       "Solver for University Examination Timetabling "
       "using Ant Colony Optimization and Local Search";
 
-  py::class_<common::ExamSection>(m, "ExamSection")
-      .def(py::init<std::string, std::vector<std::string>>(), py::arg("code"), py::arg("students"))
-      .def_property("code", &common::ExamSection::code, &common::ExamSection::set_code)
-      .def_property("students", &common::ExamSection::students, &common::ExamSection::set_students)
-      .def_property_readonly("student_count", &common::ExamSection::student_count);
+  nb::class_<common::ExamSection>(m, "ExamSection")
+      .def(nb::init<std::string, std::vector<std::string>>(), nb::arg("code"), nb::arg("students"))
+      .def_prop_rw("code", &common::ExamSection::code, &common::ExamSection::set_code)
+      .def_prop_rw("students", &common::ExamSection::students, &common::ExamSection::set_students)
+      .def_prop_ro("student_count", &common::ExamSection::student_count);
 
-  py::class_<common::Exam>(m, "Exam")
-      .def(py::init<std::string, int, std::vector<common::ExamSection>, std::vector<int>,
+  nb::class_<common::Exam>(m, "Exam")
+      .def(nb::init<std::string, int, std::vector<common::ExamSection>, std::vector<int>,
                     std::vector<int>>(),
-           py::arg("code"), py::arg("credits"), py::arg("sections"), py::arg("feasible_slots"),
-           py::arg("feasible_rooms"))
-      .def_property("code", &common::Exam::code, &common::Exam::set_code)
-      .def_property("credits", &common::Exam::credits, &common::Exam::set_credits)
-      .def_property("sections", &common::Exam::sections, &common::Exam::set_sections)
-      .def_property("feasible_slots", &common::Exam::feasible_slots,
-                    &common::Exam::set_feasible_slots)
-      .def_property("feasible_rooms", &common::Exam::feasible_rooms,
-                    &common::Exam::set_feasible_rooms)
-      .def_property_readonly("section_count", &common::Exam::section_count)
-      .def_property_readonly("student_count", &common::Exam::student_count);
+           nb::arg("code"), nb::arg("credits"), nb::arg("sections"), nb::arg("feasible_slots"),
+           nb::arg("feasible_rooms"))
+      .def_prop_rw("code", &common::Exam::code, &common::Exam::set_code)
+      .def_prop_rw("credits", &common::Exam::credits, &common::Exam::set_credits)
+      .def_prop_rw("sections", &common::Exam::sections, &common::Exam::set_sections)
+      .def_prop_rw("feasible_slots", &common::Exam::feasible_slots,
+                   &common::Exam::set_feasible_slots)
+      .def_prop_rw("feasible_rooms", &common::Exam::feasible_rooms,
+                   &common::Exam::set_feasible_rooms)
+      .def_prop_ro("section_count", &common::Exam::section_count)
+      .def_prop_ro("student_count", &common::Exam::student_count);
 
-  py::class_<common::Room>(m, "Room")
-      .def(py::init<std::string, int, std::string, std::string>(), py::arg("code"),
-           py::arg("capacity"), py::arg("location") = "default", py::arg("type") = "default")
-      .def_property("code", &common::Room::code, &common::Room::set_code)
-      .def_property("capacity", &common::Room::capacity, &common::Room::set_capacity)
-      .def_property("location", &common::Room::location, &common::Room::set_location)
-      .def_property("type", &common::Room::type, &common::Room::set_type);
+  nb::class_<common::Room>(m, "Room")
+      .def(nb::init<std::string, int, std::string, std::string>(), nb::arg("code"),
+           nb::arg("capacity"), nb::arg("location") = "default", nb::arg("type") = "default")
+      .def_prop_rw("code", &common::Room::code, &common::Room::set_code)
+      .def_prop_rw("capacity", &common::Room::capacity, &common::Room::set_capacity)
+      .def_prop_rw("location", &common::Room::location, &common::Room::set_location)
+      .def_prop_rw("type", &common::Room::type, &common::Room::set_type);
 
-  py::class_<common::Hyperparams::Evaluation>(m, "EvalParams")
-      .def(py::init<>())
-      .def_property("penalty_decay_base", &common::Hyperparams::Evaluation::penalty_decay_base,
-                    &common::Hyperparams::Evaluation::set_penalty_decay_base);
+  nb::class_<common::Hyperparams::Evaluation>(m, "EvalParams")
+      .def(nb::init<>())
+      .def_prop_rw("penalty_decay_base", &common::Hyperparams::Evaluation::penalty_decay_base,
+                   &common::Hyperparams::Evaluation::set_penalty_decay_base);
 
-  py::class_<common::Hyperparams::ACO>(m, "AcoParams")
-      .def(py::init<>())
-      .def_property("num_iters", &common::Hyperparams::ACO::num_iters,
-                    &common::Hyperparams::ACO::set_num_iters)
-      .def_property("num_ants", &common::Hyperparams::ACO::num_ants,
-                    &common::Hyperparams::ACO::set_num_ants)
-      .def_property("alpha", &common::Hyperparams::ACO::alpha, &common::Hyperparams::ACO::set_alpha)
-      .def_property("beta", &common::Hyperparams::ACO::beta, &common::Hyperparams::ACO::set_beta)
-      .def_property("rho", &common::Hyperparams::ACO::rho, &common::Hyperparams::ACO::set_rho)
-      .def_property("tau_min", &common::Hyperparams::ACO::tau_min,
-                    &common::Hyperparams::ACO::set_tau_min)
-      .def_property("tau_max", &common::Hyperparams::ACO::tau_max,
-                    &common::Hyperparams::ACO::set_tau_max)
-      .def_property("max_retries", &common::Hyperparams::ACO::max_retries,
-                    &common::Hyperparams::ACO::set_max_retries)
-      .def("set_tau_bounds", &common::Hyperparams::ACO::set_tau_bounds, py::arg("tau_min"),
-           py::arg("tau_max"));
+  nb::class_<common::Hyperparams::ACO>(m, "AcoParams")
+      .def(nb::init<>())
+      .def_prop_rw("num_iters", &common::Hyperparams::ACO::num_iters,
+                   &common::Hyperparams::ACO::set_num_iters)
+      .def_prop_rw("num_ants", &common::Hyperparams::ACO::num_ants,
+                   &common::Hyperparams::ACO::set_num_ants)
+      .def_prop_rw("alpha", &common::Hyperparams::ACO::alpha, &common::Hyperparams::ACO::set_alpha)
+      .def_prop_rw("beta", &common::Hyperparams::ACO::beta, &common::Hyperparams::ACO::set_beta)
+      .def_prop_rw("rho", &common::Hyperparams::ACO::rho, &common::Hyperparams::ACO::set_rho)
+      .def_prop_rw("tau_min", &common::Hyperparams::ACO::tau_min,
+                   &common::Hyperparams::ACO::set_tau_min)
+      .def_prop_rw("tau_max", &common::Hyperparams::ACO::tau_max,
+                   &common::Hyperparams::ACO::set_tau_max)
+      .def_prop_rw("max_retries", &common::Hyperparams::ACO::max_retries,
+                   &common::Hyperparams::ACO::set_max_retries)
+      .def("set_tau_bounds", &common::Hyperparams::ACO::set_tau_bounds, nb::arg("tau_min"),
+           nb::arg("tau_max"));
 
-  py::class_<common::Hyperparams::LS>(m, "LsParams")
-      .def(py::init<>())
-      .def_property("prob_1_move", &common::Hyperparams::LS::prob_1_move,
-                    &common::Hyperparams::LS::set_prob_1_move)
-      .def_property("max_improvements", &common::Hyperparams::LS::max_improvements,
-                    &common::Hyperparams::LS::set_max_improvements)
-      .def_property("patience", &common::Hyperparams::LS::patience,
-                    &common::Hyperparams::LS::set_patience);
+  nb::class_<common::Hyperparams::LS>(m, "LsParams")
+      .def(nb::init<>())
+      .def_prop_rw("prob_1_move", &common::Hyperparams::LS::prob_1_move,
+                   &common::Hyperparams::LS::set_prob_1_move)
+      .def_prop_rw("max_improvements", &common::Hyperparams::LS::max_improvements,
+                   &common::Hyperparams::LS::set_max_improvements)
+      .def_prop_rw("patience", &common::Hyperparams::LS::patience,
+                   &common::Hyperparams::LS::set_patience);
 
-  py::class_<common::Hyperparams>(m, "Hyperparams")
-      .def(py::init<common::Hyperparams::Evaluation, common::Hyperparams::ACO,
+  nb::class_<common::Hyperparams>(m, "Hyperparams")
+      .def(nb::init<common::Hyperparams::Evaluation, common::Hyperparams::ACO,
                     common::Hyperparams::LS>(),
-           py::arg_v("_eval", common::Hyperparams::Evaluation(), "EvalParams()"),
-           py::arg_v("_aco", common::Hyperparams::ACO(), "AcoParams()"),
-           py::arg_v("_ls", common::Hyperparams::LS(), "LsParams()"))
-      .def_readwrite("eval", &common::Hyperparams::eval)
-      .def_readwrite("aco", &common::Hyperparams::aco)
-      .def_readwrite("ls", &common::Hyperparams::ls);
+           nb::arg("_eval") = common::Hyperparams::Evaluation(),
+           nb::arg("_aco") = common::Hyperparams::ACO(),
+           nb::arg("_ls") = common::Hyperparams::LS())
+      .def_rw("eval", &common::Hyperparams::eval)
+      .def_rw("aco", &common::Hyperparams::aco)
+      .def_rw("ls", &common::Hyperparams::ls);
 
-  py::class_<common::Solution>(m, "Solution")
-      .def_readonly("schedule", &common::Solution::assigned_slots)
-      .def_readonly("fitness", &common::Solution::fitness);
+  nb::class_<common::Solution>(m, "Solution")
+      .def_ro("schedule", &common::Solution::assigned_slots)
+      .def_ro("fitness", &common::Solution::fitness);
 
-  py::class_<aco::AntColony>(m, "AntColony")
-      .def(py::init<common::Hyperparams, const std::vector<common::Exam>&, std::vector<int64_t>,
+  nb::class_<aco::AntColony>(m, "AntColony")
+      .def(nb::init<common::Hyperparams, const std::vector<common::Exam>&, std::vector<int64_t>,
                     const std::vector<common::Room>&, uint64_t>(),
-           py::arg("hyperparams"), py::arg("exams"), py::arg("slot_timestamps"), py::arg("rooms"),
-           py::arg("base_seed") = 42)  // NOLINT(*-magic-numbers)
+           nb::arg("hyperparams"), nb::arg("exams"), nb::arg("slot_timestamps"), nb::arg("rooms"),
+           nb::arg("base_seed") = 42)  // NOLINT(*-magic-numbers)
       .def("run_one_iteration", &aco::AntColony::run_one_iteration,
-           py::call_guard<py::gil_scoped_release>())
-      .def("run", &aco::AntColony::run, py::arg("callback") = py::none(),
-           py::call_guard<py::gil_scoped_release>())
-      .def_readonly("global_best_schedule", &aco::AntColony::global_best_schedule)
-      .def_readonly("global_best_fitness", &aco::AntColony::global_best_fitness);
+           nb::call_guard<nb::gil_scoped_release>())
+      .def("run", &aco::AntColony::run, nb::arg("callback") = nb::none(),
+           nb::call_guard<nb::gil_scoped_release>())
+      .def_ro("global_best_schedule", &aco::AntColony::global_best_schedule)
+      .def_ro("global_best_fitness", &aco::AntColony::global_best_fitness);
 }
